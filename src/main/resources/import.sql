4d85ee67996551e80e4443942de075786016b777
edurxmos <eduardoramos15975@gmail.com>
2025-05-05 19:22:47 -0300

    seeding do banco de dados no import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_usuario (id, nome, email) VALUES (1, 'João Silva', 'joao@email.com');
+INSERT INTO tb_usuario (id, nome, email) VALUES (2, 'Eduardo Ramos', 'eduardo@email.com');
+INSERT INTO tb_usuario (id, nome, email) VALUES (3, 'Gabriela Melquiades', 'gabrielao@email.com');
+
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (1, 'Dom Quixote', 'Miguel de Cervantes', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (2, '1984', 'George Orwell', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (3, 'O Senhor dos Anéis', 'J.R.R. Tolkien', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (4, 'Orgulho e Preconceito', 'Jane Austen', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (5, 'Cem Anos de Solidão', 'Gabriel García Márquez', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (6, 'Harry Potter e a Pedra Filosofal', 'J.K. Rowling', true);
+INSERT INTO tb_livro (id, nome, autor, disponivel) VALUES (7, 'A Revolução dos Bichos', 'George Orwell', true);
