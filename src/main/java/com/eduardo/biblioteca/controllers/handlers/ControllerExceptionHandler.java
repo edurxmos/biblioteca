20c1ff4cbb5a91a0c79b0a1c4fb958da57709cde
edurxmos <eduardoramos15975@gmail.com>
2025-05-13 22:05:15 -0300

    customizando a resposta da validação do Bean Validation

--- src/main/java/com/eduardo/biblioteca/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/eduardo/biblioteca/controllers/handlers/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.eduardo.biblioteca.controllers.handlers;
 
 import com.eduardo.biblioteca.dtos.CustomError;
+import com.eduardo.biblioteca.dtos.ValidationError;
 import com.eduardo.biblioteca.services.exceptions.NaoEncontradoException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,4 +22,17 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
